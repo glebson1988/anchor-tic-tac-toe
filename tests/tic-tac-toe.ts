eecb049585ee8178fc4926ba4abf5b75fb61fc34
glebson1988 <glebson1988@yandex.ru>
2025-01-08 17:54:49 +0100

    Play helper for tests

--- tests/tic-tac-toe.ts
+++ tests/tic-tac-toe.ts
@@ -3,6 +3,30 @@ import { Program } from "@coral-xyz/anchor";
 import { TicTacToe } from "../target/types/tic_tac_toe";
 import { expect } from "chai";
 
+async function play(
+    program: Program<TicTacToe>,
+    game,
+    player,
+    tile,
+    expectedTurn,
+    expectedGameState,
+    exptectedBoard
+) {
+    await program.methods
+    .play(tile)
+    .accounts({
+        player: player.publicKey,
+        game,
+    })
+    .signers(player instanceof (anchor.Wallet as any) ? [] : [player])
+    .rpc()
+
+    const gameState = await program.account.game.fetch(game);
+    expect(gameState.turn).to.equal(expectedTurn);
+    expect(gameState.state).to.eql(expectedGameState);
+    expect(gameState.board).to.eql(exptectedBoard);
+}
+
 describe("tic-tac-toe", () => {
     const program = anchor.workspace.TicTacToe as Program<TicTacToe>;
     const programProvider = program.provider as anchor.AnchorProvider;
