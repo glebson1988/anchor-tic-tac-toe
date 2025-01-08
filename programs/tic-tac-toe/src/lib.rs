57d4bc207149102d2279973dbdac44f6a9ea0b86
glebson1988 <glebson1988@yandex.ru>
2025-01-08 17:47:00 +0100

    Play instruction

--- programs/tic-tac-toe/src/lib.rs
+++ programs/tic-tac-toe/src/lib.rs
@@ -11,6 +11,18 @@ pub mod tic_tac_toe {
     pub fn setup_game(ctx: Context<SetupGame>, player_two: Pubkey) -> Result<()> {
         ctx.accounts.game.start([ctx.accounts.player_one.key(), player_two])
     }
+
+    pub fn play(ctx: Context<Play>, tile: Tile) -> Result<()> {
+        let game = &mut ctx.accounts.game;
+
+        require_keys_eq!(
+            game.current_player(),
+            ctx.accounts.player.key(),
+            TicTacToeError::NotPlayersTurn
+        );
+
+        game.play(&tile)
+    }
 }
 
 impl Game {
@@ -114,6 +126,13 @@ pub struct SetupGame<'info> {
     #[account(mut)]
     pub player_one: Signer<'info>,
     pub system_program: Program<'info, System>
+}
+
+#[derive(Accounts)]
+pub struct Play<'info> {
+    #[account(mut)]
+    pub game: Account<'info, Game>,
+    pub player: Signer<'info>
 }
 
 #[account]
